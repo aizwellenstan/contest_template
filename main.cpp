3ea6b5f1cd264e3622f6482b98b2292d3f71b717
私はただただ進むだけ <aizwellenstan@gmail.com>
2026-03-28 19:57:43 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,24 @@
-%%writefile solution.cpp
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
-bool f(string& o) {
-    return 1;
+void f() {
+    ll n; 
+    // This line handles the actual problem input
+    if (!(cin >> n)) exit(0); 
+
+    // --- YOUR LOGIC HERE ---
+    // -----------------------
+
+    cout << ans << "\n";
 }
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
-    string o; while (f(o)); cout << o;
+    
+    int t = 1; 
+    if (cin >> t) {
+        while(t--) f(); 
+        while(cin >> t) f(); // Safety for trailing EOF data
+    }
 }
